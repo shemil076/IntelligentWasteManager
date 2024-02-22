633a6283e676af86c771fc5286e8af6a73b1a91f
shemil076 <shemil076@gmail.com>
2024-02-22 20:20:20 +0530

    Initial Commit

---
+++ InteligentWasteManager/InteligentWasteManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteligentWasteManagerApp.swift
+//  InteligentWasteManager
+//
+//  Created by PRAMUDITHA KARUNARATHNA on 2024-02-22.
+//
+
+import SwiftUI
+
+@main
+struct InteligentWasteManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
